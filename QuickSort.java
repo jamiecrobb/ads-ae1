be23201cc7171232c475d26aa9a3c662f4bf7366
Jamie Robb <jamierobb321@gmail.com>
2023-02-13 15:39:05 +0000

    Finished 1c

--- QuickSort.java
+++ QuickSort.java
@@ -1,15 +1,16 @@
+import java.util.concurrent.DelayQueue;
+
 public class QuickSort  
 {
+    // Utility method to swap two elements in an array
+    static void swap(int[] A, int p, int q)
+    {
+        int temporary = A[p];
+        A[p] = A[q];
+        A[q] = temporary;
+    }
+
     // Part 1, a
-    // PARTITION(A,p,r)
-    // x := A[r]
-    // i := p – 1
-    // for j = p to r - 1
-    //     if A[j] <= x
-    //         i := i + 1
-    //         SWAP(A[i],A[j])
-    // SWAP(A[i+1],A[r])
-    // return i + 1
     static int Partition(int[] A, int p, int r)
     {
         int x = A[r];
@@ -19,25 +20,13 @@ public class QuickSort
             if (A[j] <= x)
             {
                 i++;
-                int ai = A[i];
-                int aj = A[j];
-                A[i] = aj;
-                A[j] = ai;
+                swap(A, i, j);
             }
         }
-        int ai1 = A[i+1];
-        int ar = A[r];
-        A[i+1] = ar;
-        A[r] = ai1;
+        swap(A, i+1, r);
         return i + 1;
     }
 
-
-    // QUICKSORT(A,p,r)
-    //   if p < r
-    //     q := PARTITION(A,p,r)
-    //     QUICKSORT(A,p,q-1)
-    //     QUICKSORT(A,q+1,r)
     static void Quicksort(int[] A, int p, int r)
     {
         if (p < r)
@@ -48,6 +37,7 @@ public class QuickSort
         }
     }
 
+    // Part 1, b
     public static void InsertionSort(int[] listToSort)
     {
         for (int j = 1; j < listToSort.length; j++)
@@ -63,63 +53,124 @@ public class QuickSort
         }
     }
 
-    // Part 1, b
-    // QUICKSORT (with INSERTION-SORT)
-    // When calling QUICKSORT on a subarray with fewer than k elements, 
-    // return without sorting the subarray
-    // After the top-level call to QUICKSORT returns, 
-    // run INSERTION-SORT on the entire array to finish the sorting process
     static void QuicksortInsertion(int[] A, int p, int r, int k)
     {
         if ((r - p) < k)
         {
-            // DO NOTHING??
-            System.out.print("");
+            ;
         }
         else if (p < r)
         {
             int q = Partition(A, p, r);
+            QuicksortInsertion(A, p, q-1, k);
+            QuicksortInsertion(A, q+1, r, k);
+        }
+        InsertionSort(A);
+    }
+
+    // Part 1c
+    static void sortElements(int[] A, int left, int right)
+    {
+        // Sort the left, right and middle elements
+        int middle = (right + left) / 2;
+        if (A[left] > A[middle])
+        {
+            swap(A, left, middle);
+        }
+        if (A[left] > A[right])
+        {
+            swap(A, left, right);
+        }
+        if (A[middle] > A[right])
+        {
+            swap(A, middle, right);
+        }
+        // Make the median element the second-to-right element
+        swap(A, middle, right - 1);
+    }
+
+    static void QuicksortMedianOfThree(int[] A, int p, int r)
+    {
+        if (p < r)
+        {
+            sortElements(A, p, r);
+            int q = Partition(A, p, r);
             Quicksort(A, p, q-1);
             Quicksort(A, q+1, r);
         }
-        InsertionSort(A);
+    }
+
+    // Part 1d
+    static void ThreeWayQuicksort()
+    {
+
     }
 
 
     public static void main(String[] args)
     {
-        // Part 1a
-        int[] arr = new int[]{3,2,5,1,4,8,6,-1};
-        System.out.println("Unsorted list:");
-        for (int i : arr)
-        {
-            System.out.print(i + " ");
-        }
-        System.out.println("\n\nPart 1A: Sorted list: ");
-        Quicksort(arr, 0, arr.length - 1);
-        for (int i : arr)
-        {
-            System.out.print(i + " ");
-        }
-        
-        // Part 1b
-        int[] arr2 = new int[]{3,2,5,1,4,8,6,-1};
-        System.out.println("\n\nPart 1A: Sorted list: ");
-        QuicksortInsertion(arr2, 0, arr2.length - 1, 3);
-        for (int i : arr2)
+        // // Part 1a
+        // System.out.println("Part 1a:");
+        // int[] arr = new int[]{3,2,5,1,4,8,6,-1};
+        // for (int i : arr)
+        // {
+        //     System.out.print(i + " ");
+        // }
+        // System.out.println();
+        // Quicksort(arr, 0, arr.length - 1);
+        // for (int i : arr)
+        // {
+        //     System.out.print(i + " ");
+        // }
+        // System.out.println();
+        // System.out.println("----------------------------------------------------------------");
+
+        // // Part 1b
+        // System.out.println("Part 1b:");
+        // int[] arr2 = new int[]{3,2,5,1,4,8,6,-1};
+        // for (int i : arr2)
+        // {
+        //     System.out.print(i + " ");
+        // }
+        // System.out.println();
+        // Quicksort(arr2, 0, arr2.length - 1);
+        // for (int i : arr2)
+        // {
+        //     System.out.print(i + " ");
+        // }
+        // System.out.println();
+        // System.out.println("----------------------------------------------------------------");
+
+        // // Part 1c
+        // System.out.println("Part 1c:");
+        // int[] arr3 = new int[]{3,2,5,1,4,8,6,-1};
+        // for (int i : arr3)
+        // {
+        //     System.out.print(i + " ");
+        // }
+        // System.out.println();
+        // QuicksortMedianOfThree(arr3, 0, arr3.length - 1);
+        // for (int i : arr3)
+        // {
+        //     System.out.print(i + " ");
+        // }
+        // System.out.println();
+        // System.out.println("----------------------------------------------------------------");
+
+        // Part 1d
+        System.out.println("Part 1d:");
+        int[] arr4 = new int[]{3,2,5,1,4,8,6,-1};
+        for (int i : arr4)
         {
             System.out.print(i + " ");
         }
         System.out.println();
+        ThreeWayQuicksort(arr4, 0, arr4.length - 1);
+        for (int i : arr4)
+        {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("----------------------------------------------------------------");
     }
-
-
-
-
-    // Part 1, c
-    // Median-of-three PARTITION
-
-
-    // Part 1, d
-    // 3-WAY-QUICKSORT
 }  
